4986562d9900c50b36e0ca295365c499dda6143b
Imran Ahmed <ahmedi@ynap.world>
2024-12-10 14:44:13 +0530

    Indent tabs to 4 spaces

--- Project/Lite.pm
+++ Project/Lite.pm
@@ -15,10 +15,10 @@ our $backup_dir="$ENV{'HOME'}/.Backup_d/";
 #$backup_dir=~ s|Diary|Backup_d|;
 
 sub new(){
-	my $class=shift;
-	my $self={};
-	bless $self,$class;
-	
+    my $class=shift;
+    my $self={};
+    bless $self,$class;
+    
 }
 =pod
 
@@ -27,7 +27,7 @@ Validate date to check the context.
 =cut
 sub get_and_validate()
 {
-	my $date=$_[1];
+    my $date=$_[1];
         if($_[0]->isvalid_date($date)){
                 $date=~ s/\/(.*?)\//"_".$month{$1}."_"/e;
                 chomp($date);
@@ -42,7 +42,7 @@ sub get_and_validate()
 
 
 sub isvalid_date(){
-	($_[1]=~ /^(([0-9]|[0-2][0-9]|3[0-1])\/0?(1|3|5|7|8|10|12)|([0-9]|[0-2][0-9]|30)\/0?(4|6|9|11)|([0-9]|[0-2][0-9]\/0?2))\/20[1-3][0-9]$/)? (return 1) :  (return 0);
+    ($_[1]=~ /^(([0-9]|[0-2][0-9]|3[0-1])\/0?(1|3|5|7|8|10|12)|([0-9]|[0-2][0-9]|30)\/0?(4|6|9|11)|([0-9]|[0-2][0-9]\/0?2))\/20[1-3][0-9]$/)? (return 1) :  (return 0);
 }
 
 =pod
@@ -52,17 +52,17 @@ Add Text in your diary
 =cut
 
 sub add_text(){
-	my $text=$_[1];
-	my ($month,$day,$time, $year)=`date` =~ /\s(.*?)\s+?(\d+)\s*?(\d+\:\d+\:\d+).*?(\d+)$/;
-	my $todays_file="$Diary_dir/".$day."_".$month."_".$year.".txt";
-	mkpath($Diary_dir);mkpath($backup_dir);
-	my $linkfile="$backup_dir/".$day."_".$month."_".$year.".txt";
-	open(FH,">>$todays_file") or die "Could not open file!";
-	if(! -e $linkfile){		
-		`ln $todays_file  $linkfile`;
-	}
-	print FH "TIME:: $time\n$text\n\n";
-	close(FH);
+    my $text=$_[1];
+    my ($month,$day,$time, $year)=`date` =~ /\s(.*?)\s+?(\d+)\s*?(\d+\:\d+\:\d+).*?(\d+)$/;
+    my $todays_file="$Diary_dir/".$day."_".$month."_".$year.".txt";
+    mkpath($Diary_dir);mkpath($backup_dir);
+    my $linkfile="$backup_dir/".$day."_".$month."_".$year.".txt";
+    open(FH,">>$todays_file") or die "Could not open file!";
+    if(! -e $linkfile){        
+        `ln $todays_file  $linkfile`;
+    }
+    print FH "TIME:: $time\n$text\n\n";
+    close(FH);
 }
 
 
@@ -76,72 +76,72 @@ use Time::Local;
 
 
 sub new(){
-	my $class=shift;
-	my $self={};
-	bless $self,$class;
+    my $class=shift;
+    my $self={};
+    bless $self,$class;
 
 }
 
 sub combine_files(){
-	my ($self,$filename)=@_;
-	chdir("$Project::Lite::Diary_dir");
+    my ($self,$filename)=@_;
+    chdir("$Project::Lite::Diary_dir");
         my @all_files;
         eval{   @all_files=`ls -l * | awk -F " " '{print \$9};'`;};
         if($@){print STDOUT "No Files till date\n";}
-	my %all_files_trimmed=map{ $_=>&remove_whitespace($_) } @all_files;
-      	my %manipulated_files=map{ my $val=$_;$_=>&mymanipulate($val) } %all_files_trimmed;
-	if(-e $filename){
-		die "File aleady Exists";
-	}
-	print STDOUT ("saved as $filename\n");
-	my @dates=values %manipulated_files;
-	my @sort_dates=&sort_dates(@dates);
-	
-	foreach my $date(@sort_dates){
-		while (my ($key,$value)= each %manipulated_files){
-			next if($date ne $value);
-			open(FH,"<$key") or die "$!";
-			open(CH,">>$filename") or die "$!";
-			print CH "DATE:  ".$value."\n\n";
-			while(<FH>){
-				print CH $_;
-			}
-			close(CH);
-			close(FH);
-		}
-	}
-	system("unix2dos $filename");
-	sub remove_whitespace(){
-		$_[0]=~ s/\n//g;
-		$_[0]=~ s/^\s+|\s+$//g;
-		return $_[0];
-	}
+    my %all_files_trimmed=map{ $_=>&remove_whitespace($_) } @all_files;
+          my %manipulated_files=map{ my $val=$_;$_=>&mymanipulate($val) } %all_files_trimmed;
+    if(-e $filename){
+        die "File aleady Exists";
+    }
+    print STDOUT ("saved as $filename\n");
+    my @dates=values %manipulated_files;
+    my @sort_dates=&sort_dates(@dates);
+    
+    foreach my $date(@sort_dates){
+        while (my ($key,$value)= each %manipulated_files){
+            next if($date ne $value);
+            open(FH,"<$key") or die "$!";
+            open(CH,">>$filename") or die "$!";
+            print CH "DATE:  ".$value."\n\n";
+            while(<FH>){
+                print CH $_;
+            }
+            close(CH);
+            close(FH);
+        }
+    }
+    system("unix2dos $filename");
+    sub remove_whitespace(){
+        $_[0]=~ s/\n//g;
+        $_[0]=~ s/^\s+|\s+$//g;
+        return $_[0];
+    }
 
 }
 
 
 sub show_index(){
-	chdir("$Project::Lite::Diary_dir");
-	my @all_files;
-	eval{	@all_files=`ls -l * | awk -F " " '{print \$NF};'`;};
+    chdir("$Project::Lite::Diary_dir");
+    my @all_files;
+    eval{    @all_files=`ls -l * | awk -F " " '{print \$NF};'`;};
 
-	if($@){print STDOUT "No Files till date\n";}
-	my @manipulated_files=map(&mymanipulate($_),@all_files);
-	my @new_all_files=&sort_dates(@manipulated_files);
-	my $value;	
-	for(@new_all_files){
-		my $val=$_;
-		$_=~ s/\//_/g;$_=~ s/\_(\d+)\_/\_$month{$1}\_/;$_=~ s/$/.txt/;	
-		my $headlines=&capture_headlines($_);
-		($value)? ($value=$value."\n".$val."\n".$headlines) : ($value=$val."\n".$headlines);
+    if($@){print STDOUT "No Files till date\n";}
+    my @manipulated_files=map(&mymanipulate($_),@all_files);
+    my @new_all_files=&sort_dates(@manipulated_files);
+    my $value;    
+    for(@new_all_files){
+        my $val=$_;
+        $_=~ s/\//_/g;$_=~ s/\_(\d+)\_/\_$month{$1}\_/;$_=~ s/$/.txt/;    
+        my $headlines=&capture_headlines($_);
+        ($value)? ($value=$value."\n".$val."\n".$headlines) : ($value=$val."\n".$headlines);
 
-	}
-	print  STDERR $value."\n";
+    }
+    print  STDERR $value."\n";
 }
 
 sub sort_dates(){
         my (@arr)=@_;
-	no warnings;
+    no warnings;
         my (%so,@so,@sorted_dates);
         %so=map{ &settime($_)=> $_} @arr;
         @so=sort{ $a <=> $b } keys %so;
@@ -153,7 +153,7 @@ sub sort_dates(){
                 $month=~ s/\///g;
                 $month--;
                 my $time;
-              	eval{
+                  eval{
                         $time = timelocal(0,0,0,$date,$month,$year);
                 };
                 return $time;
@@ -165,36 +165,36 @@ sub sort_dates(){
 
 
 sub mymanipulate($){
-	my $file=$Diary_dir.$_[0];
-	$_[0]=~ s/^(\n|\s)|(\s|\n)$//g;
-	return if($_[0]!~ /^\d+\_[A-Z,a-z]{3,4}\_\d{4}\.txt$/);
-	$_[0]=~ s/_/\//g;
-	my %key_list_by_value;
-	while(my ($key,$value)= each %month ){
-		push @{$key_list_by_value{$value}}, $key;
-	}
-	$_[0]=~ s/.txt//;
-	$_[0]=~ s/([A-Z,a-z]{3,4})/$key_list_by_value{"$1"}[0]/e;
-	my $value=$_[0];
-	return $value;
+    my $file=$Diary_dir.$_[0];
+    $_[0]=~ s/^(\n|\s)|(\s|\n)$//g;
+    return if($_[0]!~ /^\d+\_[A-Z,a-z]{3,4}\_\d{4}\.txt$/);
+    $_[0]=~ s/_/\//g;
+    my %key_list_by_value;
+    while(my ($key,$value)= each %month ){
+        push @{$key_list_by_value{$value}}, $key;
+    }
+    $_[0]=~ s/.txt//;
+    $_[0]=~ s/([A-Z,a-z]{3,4})/$key_list_by_value{"$1"}[0]/e;
+    my $value=$_[0];
+    return $value;
 }
 
 sub capture_headlines(){
-	my $file=shift;
-	open(FH,$file);
-	my @headlines;
-	while(<FH>){chomp($_);
-		if($_=~ /^#/){
-			push(@headlines,$_);
-		}	
-	}
-	@headlines=map(&format($_),@headlines);
-	my $headlines=join("\n",@headlines);
-	sub format(){
-		$_[0]=~ s/\#\s*|\s+$//;
-		return '-'.$_[0];
-	}
-	return $headlines;
+    my $file=shift;
+    open(FH,$file);
+    my @headlines;
+    while(<FH>){chomp($_);
+        if($_=~ /^#/){
+            push(@headlines,$_);
+        }    
+    }
+    @headlines=map(&format($_),@headlines);
+    my $headlines=join("\n",@headlines);
+    sub format(){
+        $_[0]=~ s/\#\s*|\s+$//;
+        return '-'.$_[0];
+    }
+    return $headlines;
 }
 
 
--- main.pl
+++ main.pl
@@ -14,9 +14,9 @@ use Switch;
 
 my $object=Project::Lite->new();
 if(!-e $Diary_dir){
-	print STDOUT "New Machine!!\n";
-	mkpath($Diary_dir);
-	mkpath($backup_dir);
+    print STDOUT "New Machine!!\n";
+    mkpath($Diary_dir);
+    mkpath($backup_dir);
 }
 chdir($Diary_dir);
 my $lat_diary_dir;
@@ -24,41 +24,41 @@ my %display;
 my @diaries=`ls -l | grep "^d" | awk -F " " '{print \$NF}'`;
 #print Dumper @diaries;exit;
 if($#diaries==-1) { 
-	print STDOUT "No indexed Diary right now\nPlease enter your Diary Name\n";
-	my $diary_name=<STDIN>;
-	chomp($diary_name);
-	if($diary_name){
-		$lat_diary_dir="$Diary_dir".$diary_name;
-		print($lat_diary_dir);
-		mkpath($lat_diary_dir);
-	}
+    print STDOUT "No indexed Diary right now\nPlease enter your Diary Name\n";
+    my $diary_name=<STDIN>;
+    chomp($diary_name);
+    if($diary_name){
+        $lat_diary_dir="$Diary_dir".$diary_name;
+        print($lat_diary_dir);
+        mkpath($lat_diary_dir);
+    }
 }
 else{
-	my $i=1;
-	%display=map {$i++ => &mychomp($_)} @diaries;
-	$display{$i}="Start new diary";
-	my $statement="";
-	$statement=$statement.$_.".".$display{$_}."\n" for sort keys %display;
-	print STDOUT "Choose the following options\n$statement";
-	my $dopt=<STDIN>;
-	chomp($dopt);
-	if(defined $display{$dopt} && $display{$dopt} ne "Start new diary"){
-		$lat_diary_dir=$Diary_dir.$display{$dopt};
-		print STDOUT "You have selected '".$display{$dopt}."'.\n";
-	}
-	elsif(defined $display{$dopt} && $display{$dopt} eq "Start new diary"){
-		print STDOUT "Please enter your Diary Name\n";
-	        my $diary_name=<STDIN>;
-        	chomp($diary_name);
-	        if($diary_name){
-        	        $lat_diary_dir="$Diary_dir".$diary_name;
-	                mkpath($lat_diary_dir);
-        	}
+    my $i=1;
+    %display=map {$i++ => &mychomp($_)} @diaries;
+    $display{$i}="Start new diary";
+    my $statement="";
+    $statement=$statement.$_.".".$display{$_}."\n" for sort keys %display;
+    print STDOUT "Choose the following options\n$statement";
+    my $dopt=<STDIN>;
+    chomp($dopt);
+    if(defined $display{$dopt} && $display{$dopt} ne "Start new diary"){
+        $lat_diary_dir=$Diary_dir.$display{$dopt};
+        print STDOUT "You have selected '".$display{$dopt}."'.\n";
+    }
+    elsif(defined $display{$dopt} && $display{$dopt} eq "Start new diary"){
+        print STDOUT "Please enter your Diary Name\n";
+            my $diary_name=<STDIN>;
+            chomp($diary_name);
+            if($diary_name){
+                    $lat_diary_dir="$Diary_dir".$diary_name;
+                    mkpath($lat_diary_dir);
+            }
 
-	}
-	else{
-		print STDOUT "Sorry! You have entered wrong options\n";
-	}
+    }
+    else{
+        print STDOUT "Sorry! You have entered wrong options\n";
+    }
 };
 $Project::Lite::Diary_dir=$lat_diary_dir."/";
 my $temp=$Project::Lite::Diary_dir;
@@ -70,40 +70,40 @@ print STDOUT "Enter Your options\n1.Enter into your Diary\n2.Show my Diary\n3.Sa
 my $opt=<STDIN>;
 
 if($opt == '1'){
-	system("clear");
-	print STDOUT "Please Enter Text to add up in your Diary(Recommended: Start with headlines initiated with '#')\n";
-	my $text="";
-	while(<STDIN>){
-		last if /^q$/;
-		$text .= $_;
-	}
-	chomp($text);
-	$object->add_text($text);
-	goto MAIN;
+    system("clear");
+    print STDOUT "Please Enter Text to add up in your Diary(Recommended: Start with headlines initiated with '#')\n";
+    my $text="";
+    while(<STDIN>){
+        last if /^q$/;
+        $text .= $_;
+    }
+    chomp($text);
+    $object->add_text($text);
+    goto MAIN;
 }
 elsif($opt == '2'){
-	system("clear");
-	print STDOUT "Here is the index of your Diary\n";
-	my $index=Project::Index->new();
-	$index->show_index();
-	print STDOUT "Please Enter date to be shown in your Diary\n";
-	my $date=<STDIN>;
-	$object->get_and_validate($date);
-	goto MAIN;
+    system("clear");
+    print STDOUT "Here is the index of your Diary\n";
+    my $index=Project::Index->new();
+    $index->show_index();
+    print STDOUT "Please Enter date to be shown in your Diary\n";
+    my $date=<STDIN>;
+    $object->get_and_validate($date);
+    goto MAIN;
 }
 elsif($opt == '3'){
-	print STDOUT "Enter Name of the file\n";
-	my $filename=<STDIN>;
-	chomp($filename);
-	my $index=Project::Index->new();
+    print STDOUT "Enter Name of the file\n";
+    my $filename=<STDIN>;
+    chomp($filename);
+    my $index=Project::Index->new();
         $filename=$PWD."/".$filename;
-	$index->combine_files("$filename");
-	goto MAIN;
+    $index->combine_files("$filename");
+    goto MAIN;
 }
 elsif($opt == '4'){exit;}
 else{
 
-	goto MAIN;
+    goto MAIN;
 
 }
 sub mychomp($){$_[0]=~ s/^(\n|\s)|(\s|\n)$//g;return $_[0];}
